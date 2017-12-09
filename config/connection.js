e0470ccf4531b2270420ae440ee928a2e830d81c
omes000 <omes000@gmail.com>
2017-12-09 10:51:21 -0600

    updating db stuff

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-	host: "us-cdbr-iron-east-05.cleardb.net",
-	user: "bb2e9e42d9ac4f",
-	password: "973cf755",
-	database: "heroku_3af688aac6389e7"
-});
+var connection;
+if(process.env.CLEARDB_URL){
+	connection = mysql.createConnection(process.env.CLEARDB_URL);
+}else{
+	connection = mysql.createConnection({
+		host: "us-cdbr-iron-east-05.cleardb.net",
+		user: "bb2e9e42d9ac4f",
+		password: "973cf755",
+		database: "heroku_3af688aac6389e7"
+	});
+}
+
 
 connection.connect(function(error){
 	if(error){
